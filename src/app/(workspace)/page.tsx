e1ea2435bd6dddb1958b7e7da7e6db16f639b8dd
Hasnain Khan <mohasnainkhan148@gmail.com>
2026-02-16 10:42:18 +0530

    04 Header

--- src/app/(workspace)/page.tsx
+++ src/app/(workspace)/page.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import { currentUser } from "./modules/authentication/actions";
-import UserButton from "./modules/authentication/components/user-button";
+import { currentUser } from "../modules/authentication/actions";
+import UserButton from "../modules/authentication/components/user-button";
 
 export default async function Home() {
   const user = await currentUser();
