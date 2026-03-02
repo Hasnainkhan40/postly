be67cc715ebb627447ad77f6eafb20b4eff3da6a
Hasnain Khan <mohasnainkhan148@gmail.com>
2026-03-02 09:41:17 +0530

    update

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,18 +5,10 @@ import { env } from "./env";
 // If your Prisma file is located elsewhere, you can change the path
 
 export const auth = betterAuth({
-    database: prismaAdapter(db, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
-    }),
-
-    socialProviders: {
-        github: {
-            clientId:env.GITHUB_CLIENT_ID!,
-            clientSecret: env.GITHUB_CLIENT_SECRET!,
-        },
-        google: {
-            clientId:env.GOOGLE_CLIENT_ID!,
-            clientSecret: env.GOOGLE_CLIENT_SECRET!,
-        }
-    }
+  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  database: prismaAdapter(db, { provider: "postgresql" }),
+  socialProviders: {
+    github: { clientId: env.GITHUB_CLIENT_ID!, clientSecret: env.GITHUB_CLIENT_SECRET! },
+    google: { clientId: env.GOOGLE_CLIENT_ID!, clientSecret: env.GOOGLE_CLIENT_SECRET! },
+  },
 });
